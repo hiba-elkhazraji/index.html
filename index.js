85a337513be2069845aedae6d0f506e3f9ca1385
hiba-elkhazraji <hiponita08@gmail.com>
2025-08-23 12:35:04 +0100

    added API

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-function showAnswer(response) {}
+function showAnswer(response) {
+  alert(response.data.answer);
+}
 let apiKey = "cf91d38a44tc474fa778f9df1bo2b7a0";
-let context = "be polite";
-let prompt = "when was the first AI created";
+let context = "be polite and provide a very short answer";
+let prompt = "who is the first female president?";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+console.log("processing");
 axios.get(apiUrl).then(showAnswer);
